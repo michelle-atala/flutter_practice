258c16ccf543f7c744b4f7704a4c2f58057651f3
Michelle.atala <atalamchll7@gmail.com>
2022-09-24 22:29:44 +0300

    login

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -6,109 +7,125 @@ class LoginPage extends StatefulWidget {
   State<LoginPage> createState() => _LoginPageState();
 }
 
+final _emailController = TextEditingController();
+final _passwordController = TextEditingController();
+
+Future SignIn() async {
+  await FirebaseAuth.instance.signInWithEmailAndPassword(
+    email: _emailController.text.trim(),
+    password: _passwordController.text.trim(),
+  );
+}
+
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightBlue,
-      body: SafeArea(
+        backgroundColor: Colors.lightBlue,
+        body: SafeArea(
           child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center, 
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children:[
-              Image.asset('assets/StuAtt.jpeg', height: 150,width: 150,),]
-             
-          ),
-          
-          SizedBox(
-            height: 15.0,
-          ),
-          Text(
-            'STUATT',
-            style: TextStyle(
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.black),
-          ),
-          SizedBox(
-            height: 15.0,
-          ),
-          Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 350.0),
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.white),
-                    borderRadius: BorderRadius.circular(15.0)),
-                child: Padding(
-                  padding: EdgeInsets.only(left: 15.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                        border: InputBorder.none, hintText: 'Email'),
-                  ),
+            //  child: SingleChildScrollView(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Image.asset(
+                  'assets/StuAtt.jpeg',
+                  height: 150,
+                  width: 150,
                 ),
-              )),
-          SizedBox(
-            height: 12.0,
-          ),
-          Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 350.0),
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.white),
-                    borderRadius: BorderRadius.circular(15.0)),
-                child: Padding(
-                  padding: EdgeInsets.only(left: 15.0),
-                  child: TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        border: InputBorder.none, hintText: 'Password'),
-                  ),
-                ),
-              )),
-          SizedBox(
-            height: 12,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 450.0),
-            child: Container(
-              padding: const EdgeInsets.all(13.0),
-              decoration: BoxDecoration(
-                color: Colors.green,
-                border: Border.all(color: Colors.black),
-                borderRadius: BorderRadius.circular(10.0),
+              ]),
+              SizedBox(
+                height: 15.0,
               ),
-              child: Center(
-                child: Text(
-                  'Sign In',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 15.0),
-                ),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 12,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Don\'t have an account?'),
               Text(
-                ' Register Here',
-                style:
-                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                'STUATT',
+                style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 350.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(15.0)),
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 15.0),
+                      child: TextField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                            border: InputBorder.none, hintText: 'Email'),
+                      ),
+                    ),
+                  )),
+              SizedBox(
+                height: 12.0,
+              ),
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 350.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(15.0)),
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 15.0),
+                      child: TextField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            border: InputBorder.none, hintText: 'Password'),
+                      ),
+                    ),
+                  )),
+              SizedBox(
+                height: 12,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 450.0),
+                child: GestureDetector(
+                  onTap: SignIn,
+                  child: Container(
+                    padding: const EdgeInsets.all(13.0),
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      border: Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Sign In',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Don\'t have an account?'),
+                  Text(
+                    ' Register Here',
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.bold),
+                  )
+                ],
               )
-            ],
-          )
-        ]),
-      )),
-    );
+            ]),
+          ),
+        ) //),
+        );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'loginpage.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+    
+    runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
