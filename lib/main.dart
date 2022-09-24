c719dee5abc17d7b1bf15a6eba8f2442cddff220
Michelle.atala <atalamchll7@gmail.com>
2022-09-24 15:50:05 +0300

    LOGIN PAGE

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,21 @@
 import 'package:flutter/material.dart';
 
+import 'loginpage.dart';
+
 void main() {
-  runApp(const Myfirst());
+  runApp(MyApp());
 }
 
-class Myfirst extends StatelessWidget {
-  const Myfirst({super.key});
+class MyApp extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Hello World'),
-            backgroundColor: Colors.blue,
-          ),
-          body: Module_1()),
-
-
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
     );
   }
-}
-
-class Module_1 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(32),
-      child: const Text('Country',
-          style: const TextStyle(
-              color: Colors.purple,
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold)),
-      height: 50.0,
-      decoration: BoxDecoration(
-          color: Colors.green, borderRadius: BorderRadius.circular(4.0)),
-      margin: const EdgeInsets.symmetric(horizontal: 15.0),
-    );
-  }
-}  
+} 
 
 
 
